9e87ade19bcb4e2c1c3a37ff93d2f40cf8f30497
FrankBouwman <bouwman.frank@gmail.com>
2018-04-01 22:32:45 +0200

    Added comments

--- sw/airborne/modules/course2018/opencv_visual_sonar.cpp
+++ sw/airborne/modules/course2018/opencv_visual_sonar.cpp
@@ -35,27 +35,28 @@ using namespace std;
 using namespace cv;
 #include "modules/computer_vision/opencv_image_functions.h"
 
-float aggression = 0.75;
-uint8_t screen_height = 240;
-uint16_t min_square_height = 45;
-uint16_t max_square_height = 170;
-uint16_t pitch_to_pix = 125;
+float aggression = 0.75;																						//Aggression factor
+uint8_t screen_height = 240;																					//Height of the screen in pixels
+uint16_t min_square_height = 45;																				//Square height at screen horizon
+uint16_t max_square_height = 170;																				//Square height at bottom of the screen
+uint16_t pitch_to_pix = 125;																					//Effect of pitch on screen pixel rotation
 
 //Calculates the height of a pixel square
 uint16_t func_square_height(uint16_t pos)
 {
 	if (pos==0){pos=1;}
-	float theta = stateGetNedToBodyEulers_f()->theta;
-	uint16_t square_height = (float)max_square_height-((float)max_square_height-(float)min_square_height)/((float)screen_height/2.)*((float)pos+(float)pitch_to_pix*theta);
+	float theta = stateGetNedToBodyEulers_f()->theta;															//Pitch angle in radians
+	uint16_t square_height = (float)max_square_height-((float)max_square_height-(float)min_square_height)
+			/((float)screen_height/2.)*((float)pos+(float)pitch_to_pix*theta);									//Linear function for square height
 	return square_height;
 }
 
 // Calculates the number of accessible pixels in a square using the integral image of a masked image
 uint16_t number_positives_square(Mat integral_mask, uint16_t left, uint16_t right, uint16_t top, uint16_t bottom)
 {
-	uint16_t n_positives; 									//number of positive pixels in a square block
+	uint16_t n_positives; 																						//Number of positive pixels in a square block
 	n_positives = (integral_mask.at<uint32_t>(top,left)+integral_mask.at<uint32_t>(bottom,right)) -
-				  (integral_mask.at<uint32_t>(top,right)+integral_mask.at<uint32_t>(bottom,left));   //(y,x)
+				  (integral_mask.at<uint32_t>(top,right)+integral_mask.at<uint32_t>(bottom,left));   			//Select block from integral image
 
 	return n_positives;
 }
@@ -64,38 +65,25 @@ uint16_t number_positives_square(Mat integral_mask, uint16_t left, uint16_t righ
 uint16_t pixels_to_go(Mat mask, uint8_t square_width = square_width, float threshold = square_th)
 {
 	//Define masked image properties
-	int w = mask.size().width; 								//Width of mask in pixels
-	int h = mask.size().height; 							//Height of mask in pixels
-	uint16_t left_pos; 										//Left position of pixel block
-	Mat bin_mask;											//Define binary mask
-	Mat integral_mask; 										//Define integral image of mask
-	cv::threshold(mask, bin_mask, 127, 1, THRESH_BINARY); 	//Set threshold of mask
-	integral(bin_mask,integral_mask); 						//Define integral image of mask
-
-	//Decides to go left or right based on positive pixels
-	//uint16_t positives_left;
-	//uint16_t positives_right;
-	//positives_left = number_positives_square(integral_mask, 0, w-1, 0, h/2);
-	//positives_right = number_positives_square(integral_mask, 0, w-1, h/2, h-1);
-
-	//if(positives_left+positives_right< 15000){
-	//	preferred_dir=GO_RANDOM;
-	//}
-	//else{ if (positives_right>=positives_left){preferred_dir=GO_RIGHT;}
-	//else {preferred_dir=GO_LEFT;}
-	//}
+	int w = mask.size().width; 																					//Width of mask in pixels
+	int h = mask.size().height; 																				//Height of mask in pixels
+	uint16_t left_pos; 																							//Left position of pixel block
+	Mat bin_mask;																								//Define binary mask
+	Mat integral_mask; 																							//Define integral image of mask
+	cv::threshold(mask, bin_mask, 127, 1, THRESH_BINARY); 														//Set threshold of mask
+	integral(bin_mask,integral_mask); 																			//Define integral image of mask
 
 	//Step forward and check if the number of accessible pixels stays above a certain threshold
 	for(left_pos = 0; left_pos<=w+square_width; left_pos += square_width)
 	{
-		uint16_t square_height = func_square_height(left_pos);
-		uint16_t square_area = square_width*square_height; 		//Area of pixel block
-		uint16_t top_pos = (h-square_height)/2;					//Top position of pixel block
-		uint16_t bottom_pos = (h+square_height)/2;				//Bottom position of pixel block
-		uint16_t min_area = threshold*square_area;				//Minimal area of pixel block
-		uint16_t right_pos = left_pos + square_width;
-		uint16_t n_postives = number_positives_square(integral_mask, left_pos, right_pos, top_pos, bottom_pos);
-		if(n_postives < min_area)
+		uint16_t square_height = func_square_height(left_pos);													//Height of a pixel block
+		uint16_t square_area = square_width*square_height; 														//Area of pixel block
+		uint16_t top_pos = (h-square_height)/2;																	//Top position of pixel block
+		uint16_t bottom_pos = (h+square_height)/2;																//Bottom position of pixel block
+		uint16_t min_area = threshold*square_area;																//Minimal area of pixel block
+		uint16_t right_pos = left_pos + square_width;															//Bottom position of pixel block
+		uint16_t n_postives = number_positives_square(integral_mask, left_pos, right_pos, top_pos, bottom_pos);	//Number of positive pixels in a square
+		if(n_postives < min_area)																				//Check if the positive pixels is below the threshold
 		{
 			break;
 		}
@@ -106,12 +94,12 @@ uint16_t pixels_to_go(Mat mask, uint8_t square_width = square_width, float thres
 //Converts accessible pixels to accessible distance
 float pix_to_m(uint16_t pixels)
 {
-	float meters = 0.; //Initialize distance at zero, since the function is only valid for pixels>0
+	float meters = 0.; 																							//Initialize distance at zero, since the function is only valid for pixels>0
 	if (pixels>0)
 	{
-		float theta = stateGetNedToBodyEulers_f()->theta;	//Pitch angle in radians
-		meters = 0.03328*((float)pixels+(float)pitch_to_pix*theta)+2.12956;	//Calculate distance
-		meters *= aggression;
+		float theta = stateGetNedToBodyEulers_f()->theta;														//Pitch angle in radians
+		meters = 0.03328*((float)pixels+(float)pitch_to_pix*theta)+2.12956;										//Available distance
+		meters *= aggression;																					//Scale with flight aggression
 	}
 	return meters;
 }
@@ -120,37 +108,36 @@ float pix_to_m(uint16_t pixels)
 int opencv_YCbCr_filter(char *img, int width, int height)
 {
 	// Create a new image, using the original bebop image.
-	Mat M(height, width, CV_8UC2, img); //YCrCb
-	Mat M_RGB;
-	Mat masked_RGB;
+	Mat M(height, width, CV_8UC2, img); 																		//YCrCb
+	Mat M_RGB;																									//RedGreenBlue matrix
+	Mat masked_RGB;																								//Masked RedGreenBlue matrix
 
 	//Convert UYUV in paparazzi to YUV opencv
 	cvtColor(M, M_RGB, CV_YUV2RGB_Y422);
 	cvtColor(M_RGB, M, CV_RGB2YUV);
-	Mat mask(M.size(), CV_8UC1, Scalar(0));
+	Mat mask(M.size(), CV_8UC1, Scalar(0));																		//Initialize mask
 
-	// Filter YCrCb values
 	inRange(M,
 			Scalar(color_lum_min, color_cb_min, color_cr_min),
-			Scalar(color_lum_max, color_cb_max, color_cr_max),
-			mask);
+			Scalar(color_lum_max, color_cb_max, color_cr_max), mask);											// Filter YCrCb values
 
 	//Calculate distance to go
-	pix_to_go = pixels_to_go(mask);
-	m_to_go = pix_to_m(pix_to_go);
+	pix_to_go = pixels_to_go(mask);																				//Pixels to go
+	m_to_go = pix_to_m(pix_to_go);																				//Meters to go
 
-	bitwise_and(M_RGB,M_RGB,masked_RGB,mask); //in, in, out (cooy to inimg frame)
+	bitwise_and(M_RGB,M_RGB,masked_RGB,mask);																	//Copy to inimg frame (In, in, out)
 
-	uint16_t disp_pos;
-	for(disp_pos = 0; disp_pos<=pix_to_go; disp_pos += square_width)
+	//Draw vision range on screen
+	uint16_t disp_pos;																							//Screen position in pixels
+	for(disp_pos = 0; disp_pos<=pix_to_go; disp_pos += square_width)											//Loop to maximum pixel distance
 	{
-		uint16_t square_height = func_square_height(disp_pos);
-		line(masked_RGB, Point(disp_pos,(height-square_height)/2), Point(disp_pos,(height+square_height)/2), Scalar(255,0,0),1);
+		uint16_t square_height = func_square_height(disp_pos);													//Calculate square height
+		line(masked_RGB, Point(disp_pos,(height-square_height)/2),
+				Point(disp_pos,(height+square_height)/2), Scalar(255,0,0),1);									//Draw line
 	}
 
-	cvtColor(masked_RGB, M, CV_RGB2YUV);
-	//Convert back and save in original image position
-	coloryuv_opencv_to_yuv422(M, img, width, height);
+	cvtColor(masked_RGB, M, CV_RGB2YUV);																		//Convert back
+	coloryuv_opencv_to_yuv422(M, img, width, height);															//Save in original image position
 
 	return 0;
 }
